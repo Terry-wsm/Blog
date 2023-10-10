e87e651be5bf09fe90fe7fb5a1bbc8fb838a2b01
Terry-wsm <2428418774@qq.com>
2023-10-10 17:53:48 +0800

    第一次上传

---
+++ src/main/java/com/example/entity/Blog.java
@@ -0,0 +1,34 @@
+package com.example.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+public class Blog implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    private Long userId;
+
+    @NotBlank(message = "标题不能为空")
+    private String title;
+
+    @NotBlank(message = "摘要不能为空")
+    private String description;
+
+    @NotBlank(message = "内容不能为空")
+    private String content;
+
+    @JsonFormat(pattern="yyyy-MM-dd")
+    private LocalDateTime created;
+
+    private Integer status;
+}
